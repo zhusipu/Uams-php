4166acd8248ce193d174a17ef2b92c4365e1f725
LuJin <345526407@qq.com>
2019-05-16 15:43:05 +0800

    针对于v1.0  修改登录参数名错误，增加修改密码的接口

--- src/Uams/Apiv1/User.php
+++ src/Uams/Apiv1/User.php
@@ -11,6 +11,7 @@ namespace Bmzy\Uams\Apiv1;
 class User extends Base{
     const API_LOGIN = 'homev1/api/ucUserLogin';
     const API_UPDATEPASSWORD = 'homev1/Platform/updatePassword';
+    const API_RESETPASSWORD  = 'homev1/Platform/resetPassword';
     const API_WPERSON = 'homev1/api/getWPerson';
     const API_WDEPT = 'homev1/api/getWDept';
 
@@ -19,11 +20,33 @@ class User extends Base{
             'username'  =>  $username,
             'password'  =>  $password,
             'isuid' =>  $isuid,
-            'getCookie' =>  $getcookie,
+            'getcookie' =>  $getcookie,
             'ua'    =>  $ua,
             'ipaddress' =>  $ipaddress
         ];
         return $this->doPost(self::API_LOGIN, $data,[],false, true);
+    }
+
+    /**
+     * 用户重置密码
+     * @param string $returnUrl  密码修改成功后的返回地址
+     * @param string $personid  用户personId
+     * @return string 用户要跳转的地址
+     * @author lu
+     */
+    public function getResetPassword($returnUrl = '', $personid)
+    {
+        $data = array(
+            'appNo'     =>  $this->appNo,
+            'returnUrl' =>  urlencode($returnUrl),
+            'personid'  =>  $personid,
+            'sign'      =>  md5($this->appNo . $this->appKey . $returnUrl . $personid)
+        );
+        $str = '';
+        foreach ($data as $k => $v)
+            $str .= $k . '=' . $v . '&';
+        $param = rtrim($str, '&');
+        return $this->apiPrefix . self::API_RESETPASSWORD . '?' . $param;
     }
 
     public function getUpdatePassword($returnUrl='',$personid,$password){
